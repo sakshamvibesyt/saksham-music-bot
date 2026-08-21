ba2592ffaf70486e5623df8b97f2bf0ab34fddcf
sakshamvibesyt <ytsakshampartap@gmail.com>
2026-08-21 10:47:36 +0530

    Update generate_session.py

--- generate_session.py
+++ generate_session.py
@@ -1,1 +1,13 @@
+from pyrogram import Client
 
+API_ID = int(input("Enter API_ID: "))
+API_HASH = input("Enter API_HASH: ")
+
+with Client(
+    "saksham_music",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    in_memory=True
+) as app:
+    print("\nYour SESSION_STRING:\n")
+    print(app.export_session_string())
